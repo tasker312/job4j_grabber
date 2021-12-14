feb6037bcb917327bfe7c72f91aba6153d384161
tasker312 <75576562+tasker312@users.noreply.github.com>
2021-12-15 01:31:19 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -3,10 +3,39 @@ package ru.job4j.grabber;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 
-public class SqlRuParse {
-    public Post load(String url) {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SqlRuParse implements Parse {
+
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> posts = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements elements = doc.select(".postslisttopic");
+            elements.forEach(el -> {
+                String href = el.child(0).attr("href");
+                posts.add(detail(href));
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post detail(String url) {
         Post post = null;
         try {
             Document doc = Jsoup.connect(url).get();
@@ -17,7 +46,7 @@ public class SqlRuParse {
                     .replaceAll("</?b>", "");
             String created = vacancy.selectFirst(".msgFooter").text();
             created = created.substring(0, created.indexOf(':') + 3);
-            post = new Post(title, url, desc, new SqlRuDateTimeParser().parse(created));
+            post = new Post(title, url, desc, dateTimeParser.parse(created));
         } catch (Exception e) {
             e.printStackTrace();
         }
